69521b089d60bc3b6cf0c5bf73a69cf3b117e5f4
LEON <lkoudadje@gmail.com>
2026-08-14 14:55:39 +0000

    Fix: correction du nom de fichier de conversion stock et imports

--- frontend/src/pages/Logistique/boncommandeprint.jsx
+++ frontend/src/pages/Logistique/boncommandeprint.jsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from 'react';
-import conversestock from '../../services/conversestock';
+import { ConversionStockService } from '../../utils/converisonstock';
 
 /**
  * Composant BonCommandePrint
